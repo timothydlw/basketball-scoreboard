223ad91ee249c8d46ce7c8692071c790b9a2141c
timothydlw <watsonltim@gmail.com>
2026-08-03 12:54:11 -0500

    Implement buttons functionality

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ function homeOne() {
     homeScoreNumber += 1
     homeScoreEl.innerText = homeScoreNumber
 }
+
+function homeTwo() {
+    homeScoreNumber += 2
+    homeScoreEl.innerText = homeScoreNumber
+}
+
+function homeThree() {
+    homeScoreNumber += 3
+    homeScoreEl.innerText = homeScoreNumber
+}
+
+function guestOne() {
+    guestScoreNumber += 1
+    guestScoreEl.innerText = guestScoreNumber
+}
+
+function guestTwo() {
+    guestScoreNumber += 2
+    guestScoreEl.innerText = guestScoreNumber
+}
+
+function guestThree() {
+    guestScoreNumber += 3
+    guestScoreEl.innerText = guestScoreNumber
+}
